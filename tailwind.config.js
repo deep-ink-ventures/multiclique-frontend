20dbd54bb24fd3664fa49cdec81752e8abc678a2
joh@deep-ink.ventures <joh@deep-ink.ventures>
2023-09-11 20:27:24 +0800

    configure theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
+const { transparent } = require('daisyui/src/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -42,35 +44,56 @@ module.exports = {
   // daisyUI config (optional)
   daisyui: {
     styled: true,
-    themes: [ {
-      elio: {
-          "primary": '#D82B2B',
-          'primary-focus': '#781818',
-          'primary-content': '#FAFAFA',
-          "secondary": "#FF7A00",
-          'secondary-focus': '#D26400',
-          'secondary-content': '#1E1B21',
-          "accent": "#A3E635",
-          'accent-focus': '#87BB2B',
-          'accent-content': '#1E1B21',
-          'neutral': '#FAFAFA',
-          'neutral-focus': '#ABABAB',
-          'neutral-content': '#1E1B21',
-          "base-100": "#201B32",
-          'base-200': '#1A1829',
-          'base-300': '#151321',
-          'base-content': '#FAFAFA',
-          'base-container': '#050215',
-          "info": "#38BDF8",
-          'info-content': '#002B3D',
-          "success": "#A3E635",
-          'success-content': '#233800',
-          "warning": "#FB923C",
-          'warning-content': '#3D1C00',
-          "error": "#FDA4AF",
-          'error-content': '#52000A'
+    themes: [{
+      multiclique: {
+
+        // default
+        default: '#000',
+        'default-outline': transparent,
+        'default-hover': '#0D0D0D',
+        'default-active': '#000',
+        'default-disabled': '#000',
+
+
+        'primary': '#1128A2',
+        'primary-hover': '#0C1856',
+        'primary-disabled': '#1128A2',
+
+        // 'primary-focus': '#781818',
+        // 'primary-content': '#000',
+        // "secondary": "#FF7A00",
+        // 'secondary-focus': '#D26400',
+        // 'secondary-content': '#1E1B21',
+        // "accent": "#A3E635",
+        // 'accent-focus': '#87BB2B',
+        // 'accent-content': '#1E1B21',
+        // 'neutral': '#FAFAFA',
+        // 'neutral-focus': '#ABABAB',
+        // 'neutral-content': '#1E1B21',
+        "base-100": "#F5F5F5",
+        'base-200': '#fff',
+        // 'base-300': '#151321',
+        'base-content': '#000',
+        // 'base-container': '#050215',
+        // "info": "#38BDF8",
+        // 'info-content': '#002B3D',
+        // "success": "#A3E635",
+        // 'success-content': '#233800',
+        // "warning": "#FB923C",
+        // 'warning-content': '#3D1C00',
+        // "error": "#FDA4AF",
+        // 'error-content': '#52000A'
+
+        "--border-btn": '0.5px',
+        ".btn-outline": {
+          "border-color": '#C5C5C5'
+        },
+        ".btn-outline:hover": {
+          'background-color': '#f2f2f2',
+          'color': '#000'
         }
-      },
+      }
+    },
       'night'
     ],
     base: true,
@@ -78,6 +101,6 @@ module.exports = {
     logs: true,
     rtl: false,
     prefix: '',
-    darkTheme: 'elio',
+    darkTheme: 'multiclique',
   },
 };
