f136b71644303ec7bdac9bccbbccb06486d5f7e8
GHesericsu <60202455+GHesericsu@users.noreply.github.com>
2023-09-13 01:24:03 +0100

    feat: change and add more eyes friendly colors in tailwind config

--- src/components/WalletConnectModal.tsx
+++ src/components/WalletConnectModal.tsx
@@ -19,7 +19,7 @@ const WalletConnectModal = () => {
       open={isConnectModalOpen}
       confirmLoading={false}
       wrapClassName='a-modal-bg'
-      className='wallet-modal [&>div.ant-modal-content]:w-full'
+      className='wallet-modal'
       onCancel={() => {
         updateIsConnectModalOpen(false);
       }}
@@ -33,7 +33,7 @@ const WalletConnectModal = () => {
         </div>
         <div className='my-4 flex h-[300px] w-full flex-col items-center justify-center'>
           <button
-            className='btn h-16 w-[75%] text-black hover:brightness-70 hover:base-200'
+            className='hover:brightness-70 hover:base-200 btn h-16 w-[75%] text-black outline-4 !outline-black'
             name={'Freighter'}
             onClick={() => handleWalletSelect()}>
             <div className='flex w-full items-center justify-between'>
