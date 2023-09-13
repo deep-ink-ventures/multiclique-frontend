77ebfa3000d5146958753959345252146d996d7f
GHesericsu <60202455+GHesericsu@users.noreply.github.com>
2023-09-13 03:37:16 +0100

    feat: add routing to create new account

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,26 @@
+import { useRouter } from 'next/router';
+
 const Welcome = () => {
+  const router = useRouter();
+  const handleCreateNewAccount = () => {
+    // next router to /account/create
+    router.push('/account/create');
+  };
+
   return (
     <div className='flex flex-col items-center justify-center p-3'>
       <div className='my-3'>
-        <h1>Welcome to Multiclique</h1>
+        <h1>Welcome to MultiClique</h1>
       </div>
 
       <div className='my-5 flex w-full justify-around'>
         <div className='flex w-[330px] flex-col items-center justify-center rounded-lg bg-neutral p-6 shadow-xl'>
           <div className='mb-5'>Create a new Multisig Account</div>
-          <button className='btn btn-primary'>Create New Account</button>
+          <button className='btn btn-primary' onClick={handleCreateNewAccount}>
+            Create New Account
+          </button>
         </div>
+        {/* this will be a dropdown menu ?? */}
         <div className=' flex w-[330px] flex-col items-center justify-center rounded-lg bg-neutral p-5 shadow-xl'>
           <div className='mb-5'>Use an Existing Account</div>
           <button className={'btn btn-primary'}>Use Existing Account</button>
