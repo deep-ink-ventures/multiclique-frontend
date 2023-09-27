92cf3fcfa5a3b0c13b619b99d00d6f0061c8593d
joh@deep-ink.ventures <joh@deep-ink.ventures>
2023-09-27 23:23:43 +0800

    restyle mc select item and removed 'Approval' from tx step

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image';
 
 interface IAvatar {
   src?: string | StaticImport;
+  width?: number;
+  height?: number;
 }
 
-export const Avatar = ({ src = '' }: IAvatar) => {
+export const Avatar = ({ src = '', width = 120, height = 120 }: IAvatar) => {
   return (
     <div className='relative flex w-fit items-center justify-center overflow-hidden rounded-full border'>
-      <Image src={src} alt='avatar' width={120} height={120} />
+      <Image src={src} alt='avatar' width={width} height={height} />
     </div>
   );
 };
